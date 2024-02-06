bfc55a3f1646b2e55906dbe1adb44b12d8e1fc0d
Tam Trang <tuo98490@temple.edu>
2024-02-06 12:18:14 -0500

    Created adapter to display items from numberArray

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -15,24 +15,10 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 1: Populate this array */
-        val numberArray = arrayOf(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36,
-            38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82,
-            84, 86, 88, 90, 92, 94, 96, 98, 100)
-
-        // Alternate way
-//        var TempInt = 1
-//        var index = 0
-//        val numberArray = arrayOf(50)
-//        numberArray.
-//        while (TempInt <= 50){
-//            numberArray[index] = TempInt * 2
-//            TempInt++
-//            ++index
-//        }
+        val numberArray = Array(50){i -> (i+1) * 2}
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
-
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
