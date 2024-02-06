95717c3f7285947550945795e1e591ffd59054be
Tam Trang <tuo98490@temple.edu>
2024-02-06 12:01:33 -0500

    Created and populated an array of numbers

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import kotlin.text.Typography.times
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,20 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        val numberArray = arrayOf(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36,
+            38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82,
+            84, 86, 88, 90, 92, 94, 96, 98, 100)
+
+        // Alternate way
+//        var TempInt = 1
+//        var index = 0
+//        val numberArray = arrayOf(50)
+//        numberArray.
+//        while (TempInt <= 50){
+//            numberArray[index] = TempInt * 2
+//            TempInt++
+//            ++index
+//        }
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
